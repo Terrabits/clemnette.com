aa17372379878cc61e128f7aba7b2e6d0887770f
Nick Lalic <nick.lalic@gmail.com>
2016-12-09 13:42:43 -0800

    clemnette is a single page application

--- source/javascripts/_background_position.js
+++ source/javascripts/_background_position.js
@@ -31,38 +31,32 @@ function repositionBgNoTransition(x) {
 $('body').ready(function() {
   var body = $('body')[0];
   body.bgAspectRatio = 1500.0/400.0;
-  body.numBgPos      = $('nav a').length - 1;
+  body.numBgPos      = $('nav:first a').length - 1;
   $('a.navbar-brand').on('click', setBgPos(0));
   $('a#home').on('click', setBgPos(0));
 	$('a#link1').on('click', setBgPos(1));
 	$('a#link2').on('click', setBgPos(2));
 	$('a#link3').on('click', setBgPos(3));
 
-  if (window.location.hash) {
-    var hash = window.location.hash;
-    if (hash == "#Home") {
-      console.log("Home");
+  if (window.location.pathname) {
+    var pathname = window.location.pathname;
+    if (pathname == "/") {
       repositionBgNoTransition(0);
     }
-    else if (hash == "#link1") {
-      console.log("Link1");
+    else if (pathname == "/page1") {
       repositionBgNoTransition(1);
     }
-    else if (hash == "#link2") {
-      console.log("Link2");
+    else if (pathname == "/page2") {
       repositionBgNoTransition(2);
     }
-    else if (hash == "#link3") {
-      console.log("Link3");
+    else if (pathname == "/page3") {
       repositionBgNoTransition(3);
     }
     else {
-      console.log("default position");
       repositionBgNoTransition(0);
     }
   }
   else {
-    console.log('No hash?');
     repositionBgNoTransition(0);
   }
 });
